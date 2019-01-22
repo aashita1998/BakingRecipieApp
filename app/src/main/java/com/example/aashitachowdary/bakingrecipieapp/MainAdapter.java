e91f2ffb2f7d1433810245e464e5c0354e2e9b5b
ACER <aashitachowdary98@gmail.com>
2019-01-22 10:40:36 +0530

    commit changed

--- app/src/main/java/com/example/aashitachowdary/bakingrecipieapp/MainAdapter.java
+++ app/src/main/java/com/example/aashitachowdary/bakingrecipieapp/MainAdapter.java
@@ -14,6 +14,7 @@ import java.io.Serializable;
 import java.util.List;
 
 class MainAdapter extends RecyclerView.Adapter<MainAdapter.MainHolder> {
+    //This adapter is to display recipie names.
     Context context;
     List<Receipe> recipieModelList;
     List<Step> stepList;
